c8c6a6c0ee4fd3281d8b491171e535abfbdadb9d
qeed <dqamex@gmail.com>
2018-11-13 22:59:44 +0300

    refactoring

--- circ_buf.c
+++ circ_buf.c
@@ -1,54 +1,66 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "circ_buf.h"
 
 #define MAX_STRING_SIZE 512
 
 static int max_string_size = 0;
+static int current_strings_count = 0;
 static int max_strings_count = 0;
 
-int file_exists(char * filename) {
+
+int file_exists(const char * filename) {
     /* 1 - file exists; 0 - file not exists */
     return (access( filename, F_OK ) != -1);
 }
 
 int circ_find_last_entry_offset(FILE * fp) {
 	char buffer[MAX_STRING_SIZE] = "";
-	time_t max_timestamp = 0;
 	long current_offset = 0;
-	time_t timestamp = 0;
+	int index = 0;
 	while(fread(buffer, max_string_size, 1, fp) > 0) {
-		sscanf(buffer, "%ld, %s", &timestamp, buffer);	
-		if(timestamp >= max_timestamp) {
-			max_timestamp = timestamp;
+		sscanf(buffer, "%d, %s", &index, buffer);	
+		if(index >= current_strings_count) {
+			current_strings_count = index;
 			current_offset = ftell(fp);
 		}
 	}
 	return current_offset;
 }
 
+void format_fwrite(FILE * fp, const char * format, ...) {
+	char buffer[MAX_STRING_SIZE] = "";
+	va_list args;
+	va_start(args, format);
+	vsprintf(buffer, format, args);
+	va_end(args);
+	fwrite(buffer, max_string_size, 1, fp);
+}
 
 int circ_write_entry(FILE *fp, char * buffer) {
-	if(fp == NULL) return -1;
-	char new_buffer[MAX_STRING_SIZE] = "";
-	time_t timestamp = time(NULL);
-	snprintf(new_buffer, max_string_size, "%ld, %s", timestamp, buffer);
-	new_buffer[max_string_size-1] = '\n';
-	fwrite(new_buffer, max_string_size, 1, fp);
+	if(!fp) return -1;
+	format_fwrite(fp, "%ld, %s\n", (++current_strings_count), buffer);
 	return 0;
 }
 
-int circ_open_and_write(char * filename, const int strings_size, const int strings_count, char * buffer) {
-	FILE * fp = NULL;
+void create_file(const char *filename) {
+	FILE * fp = fopen(filename, "w");
+	if(fp)
+		fclose(fp);
+}
+
+int circ_open_and_write(const char * filename, const int strings_size, const int strings_count, char * buffer) {
 	max_strings_count = strings_count;
 	max_string_size = strings_size;
 	int max_size = max_strings_count * max_string_size;
-	if(!file_exists(filename)) {
-		fp = fopen(filename, "w");
-		if(fp != NULL) fclose(fp);
-	}
-	fp = fopen(filename, "r+");
+	if(!file_exists(filename)) 
+		create_file(filename);
+
+	FILE * fp = fopen(filename, "r+");
+	if(!fp) return -1;
 	int last_entry_offset = circ_find_last_entry_offset(fp);
     if( (last_entry_offset + max_string_size) > max_size ) {
         fseek(fp, 0, SEEK_SET);
@@ -56,7 +68,8 @@ int circ_open_and_write(char * filename, const int strings_size, const int strin
         fseek(fp, last_entry_offset, SEEK_SET);
     }
     circ_write_entry(fp, buffer);
-    lseek(fp, 0, SEEK_END);
+    fseek(fp, 0, SEEK_END);
     fclose(fp);
+	return 0;
 }
 
--- circ_buf.h
+++ circ_buf.h
@@ -2,16 +2,12 @@
 #define CIRC_BUF_H
 
 #include <stdio.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
 #define MAX_STRING_SIZE 512
 
 
 int circ_find_last_entry_offset(FILE * fp);
 int circ_write_entry(FILE *fp, char * buffer);
-int circ_open_and_write(char * filename, const int strings_size, const int strings_count, char * buffer);
+int circ_open_and_write(const char * filename, const int strings_size, const int strings_count, char * buffer);
 
 
 #endif
